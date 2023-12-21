1876ddbaea499787e10e0848178e723841fceb34
Aman Rahman <aman.rahman.0309@gmail.com>
2023-12-21 22:29:51 +0530

    Shaders

--- main.cpp
+++ main.cpp
@@ -7,17 +7,21 @@ void processInput(GLFWwindow* window);
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
+"	FragColor = vec4(ourColor, 1.0);\n"
+"}\n\0";
 
 int main()
 {
@@ -99,9 +103,10 @@ int main()
 
 	//Vertex Input
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		// positions			//colors
+		-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,		//bottom right
+		 0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,		//bottom left
+		 0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f,		//top
 	};
 
 	unsigned int VBO;
@@ -112,9 +117,13 @@ int main()
 	glBindVertexArray(VAO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	//color attribute	
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 
 	while(!glfwWindowShouldClose(window))
@@ -123,9 +132,14 @@ int main()
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		/*glBindVertexArray(VAO);*/
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();

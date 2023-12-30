1545c9665a308dcfc3ba11856e15c449a4e5a302
Aman Rahman <aman.rahman.0309@gmail.com>
2023-12-30 19:14:04 +0530

    Coordinate Systems(multiple cubes)

--- main.cpp
+++ main.cpp
@@ -93,6 +93,18 @@ int main()
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
+	glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 
 
 	uint32_t indices[] = {
@@ -191,26 +203,29 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		
 		//create transformations
-		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		glm::mat4 projection = glm::mat4(1.0f);	
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-		//retrieve the matrix uniform locations
-		uint32_t modelLoc = glGetUniformLocation(ourShader.ID, "model");
-		uint32_t viewLoc = glGetUniformLocation(ourShader.ID, "view");
-
-		//pass them to the shaders
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-
+		
+		//pass transformation matrices to shader
 		ourShader.setMat4("projection", projection);
+		ourShader.setMat4("view", view);
 
 		glEnable(GL_DEPTH_TEST);
 
 		glBindVertexArray(VAO);
+		for (uint32_t i = 0; i < 10; i++)
+		{
+			//calculate the model matrix for each object and pass it to the shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			ourShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glfwSwapBuffers(window);
 		glfwPollEvents();

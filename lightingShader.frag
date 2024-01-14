df7fec03474e7b020b77abeec1e4cf7417c3ee51
Aman Rahman <aman.rahman.0309@gmail.com>
2024-01-14 11:56:45 +0530

    point light

--- lightingShader.frag
+++ lightingShader.frag
@@ -12,11 +12,15 @@ struct Material
 struct Light
 {
 	// vec3 position not necessary when using directional light
-	vec3 direction;
+	vec3 position;
 
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+
+	float constant;
+	float linear;
+	float quadratic;
 };
 
 in vec3 Normal;
@@ -31,12 +35,15 @@ uniform Light light;
 
 void main()
 {
+	float distance = length(light.position - FragPos);
+	float attenuation = 1.0/(light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
 	//ambient
 	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
 	//diffuse
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(-light.direction);
+	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
@@ -46,6 +53,10 @@ void main()
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
 	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
 
 	vec3 result = ambient + diffuse + specular;
 	FragColor = vec4(result, 1.0);
--- main.cpp
+++ main.cpp
@@ -189,13 +189,16 @@ int main()
 
 		//be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.use();
-		lightingShader.setVec3("light.direction", -0.2, -1.0f, -0.3f);
+		lightingShader.setVec3("light.position", lightPos);
 		lightingShader.setVec3("viewPos", camera.Position);
 
 		//light properties
 		lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
 		lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
 		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("light.constant", 1.0f);
+		lightingShader.setFloat("light.linear", 0.09f);
+		lightingShader.setFloat("light.quadratic", 0.032f);
 
 		//material properties
 		lightingShader.setFloat("material.shininess", 32.0f);
@@ -233,17 +236,17 @@ int main()
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 
-		////also draw the lamp object
-		//lightingCubeShader.use();
-		//lightingCubeShader.setMat4("projection", projection);
-		//lightingCubeShader.setMat4("view", view);
-		//model = glm::mat4(1.0f);
-		//model = glm::translate(model, lightPos);
-		//model = glm::scale(model, glm::vec3(0.2f)); //a smaller cube
-		//lightingCubeShader.setMat4("model", model);
+		//also draw the lamp object
+		lightingCubeShader.use();
+		lightingCubeShader.setMat4("projection", projection);
+		lightingCubeShader.setMat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); //a smaller cube
+		lightingCubeShader.setMat4("model", model);
 
-		//glBindVertexArray(lightCubeVAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 36);		
+		glBindVertexArray(lightCubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);		
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();

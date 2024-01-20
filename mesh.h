016c821c33a74514e85d2aca22c3e7590c83d343
Aman Rahman <aman.rahman.0309@gmail.com>
2024-01-20 22:59:52 +0530

    updated mesh header

--- mesh.h
+++ mesh.h
@@ -43,7 +43,33 @@ public:
 		setupMesh();
 	}
 	
-	void Draw(Shader& shader);
+	void Draw(Shader& shader)
+	{
+		uint32_t diffuseNr = 1;
+		uint32_t specularNr = 1;
+		for (uint32_t i = 0; i < textures.size(); i++)
+		{
+			glActiveTexture(GL_TEXTURE0 + i);
+			std::string number;
+			std::string name = textures[i].type;
+			if (name == "texture_diffuse")
+			{
+				number = std::to_string(diffuseNr++);
+			}
+			else if (name == "texture_specular")
+			{
+				number = std::to_string(specularNr++);
+			}
+			shader.setInt(("material." + name + number).c_str(), i);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+		}
+		glActiveTexture(GL_TEXTURE0);
+
+		//draw mesh
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
 
 private:
 	//render data

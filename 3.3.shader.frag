8857791c74f5747f1ede3b5a976032a04f68825c
Aman Rahman <aman.rahman.0309@gmail.com>
2024-02-07 15:02:19 +0530

    Reflection

--- 3.3.shader.frag
+++ 3.3.shader.frag
@@ -1,13 +1,15 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
-in vec2 TexCoord;
+in vec3 Normal;
+in vec3 Position;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+uniform vec3 cameraPos;
+uniform samplerCube skybox;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+	vec3 I = normalize(Position - cameraPos);
+	vec3 R = reflect(I, normalize(Normal));
+	FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -77,54 +77,53 @@ int main()
 
     // build and compile shaders
     // -------------------------
-    Shader shader("frameBuffer.vert", "frameBuffer.frag");
+    Shader shader("3.3.shader.vert", "3.3.shader.frag");
     Shader skyboxShader("skybox.vert", "skybox.frag");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    float cubeVertices[] = {
-            // back face
-            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // bottom-left
-             0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-right
-             0.5f, -0.5f, -0.5f, 1.0f, 0.0f, // bottom-right
-             0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-right
-            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // bottom-left
-            -0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // top-left
-            // front face
-            -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-left
-             0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // bottom-right
-             0.5f,  0.5f,  0.5f, 1.0f, 1.0f, // top-right
-             0.5f,  0.5f,  0.5f, 1.0f, 1.0f, // top-right
-            -0.5f,  0.5f,  0.5f, 0.0f, 1.0f, // top-left
-            -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-left
-            // left face
-            -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-right
-            -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-left
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-left
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-left
-            -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-right
-            -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-right
-            // right face
-             0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-left
-             0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-right
-             0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-right
-             0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-right
-             0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-left
-             0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-left
-            // bottom face
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // top-right
-             0.5f, -0.5f, -0.5f, 1.0f, 1.0f, // top-left
-             0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // bottom-left
-             0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // bottom-left
-            -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-right
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // top-right
-            // top face
-            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, // top-left
-             0.5f, 0.5f,  0.5f, 1.0f, 0.0f, // bottom-right
-             0.5f, 0.5f, -0.5f, 1.0f, 1.0f, // top-right
-             0.5f, 0.5f,  0.5f, 1.0f, 0.0f, // bottom-right
-            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, // top-left
-            -0.5f, 0.5f,  0.5f, 0.0f, 0.0f // bottom-left
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
 
     float skyboxVertices[] = {
@@ -178,11 +177,11 @@ int main()
     glGenBuffers(1, &cubeVBO);
     glBindVertexArray(cubeVAO);
     glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     //skybox VAO
     uint32_t skyboxVAO, skyboxVBO;
     glGenVertexArrays(1, &skyboxVAO);
@@ -245,6 +244,7 @@ int main()
         shader.setMat4("projection", projection);
         shader.setMat4("view", view);
         shader.setMat4("model", model);
+        shader.setVec3("cameraPos", camera.Position);
         // cubes
         glBindVertexArray(cubeVAO);
         glActiveTexture(GL_TEXTURE0);
